b3caa6969185241150b662dad78c0f80fe94da79
Harshit Dixit <Harshit>
2025-07-11 11:42:01 +0530

    corrected all errors of shadow_intersection_module.py

--- shadow_intersection_module.py
+++ shadow_intersection_module.py
@@ -4,6 +4,8 @@ from astropy.coordinates import EarthLocation, AltAz, get_sun
 from astropy.time import Time
 import astropy.units as u
 import numpy as np
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def ccw(A, B, C):
     return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
@@ -40,9 +42,13 @@ obstacleCount=int(input("Enter number of obstacle"))
 
 # calculating hour angle and sun's declination angle
 
-now=Time.now()
+# convert time to local timezone (IST)
+# previously it was in UTC
+now_dt=datetime.now(ZoneInfo("Asia/Kolkata"))
+print(now_dt)
+now=Time(now_dt)
 sun=get_sun(now)
-
+print(sun)
 # now calculating sun's elevation angle
 
 
@@ -85,29 +91,40 @@ ax.set_zlim(0, 6)
 # but we have to calculate azimuth angle  with respect to each and every obstacle
 # theta is denoted as azimuth angle
 
-elevation_angle = []
-azimuth_angle = []
-for (x,y) in user_input:
-    obs_location=EarthLocation(lat=x*u.deg,lon=y*u.deg)
-    altaz=sun.transform_to(AltAz(obstime=now,location=obs_location))
-    elevation_angle.append(altaz.alt.deg)
-    if(360-altaz.az.deg<180):
-        azimuth_angle.append(360-altaz.az.deg)
-    elif(360-altaz.az.deg>=180):
-        azimuth_angle.append(altaz.az.deg)
+'''There is some issue with azimuth angle because for one object shadow is
+in different angle and for other object shadow is in different angle.
+which is incorrect because irrespective of shadow length shadow direction
+must be same
+
+solution - We were taking time as UTC previously later switched to IST
+secondly, we are not required to calculate azimuth angle and elevation angle
+for very obstacle as obstacles are very close to each other small errors can be
+neglected therefore object location won't make much difference in elevation
+and azimuth angles
+'''
+
+'''
+next issue - The size of the shadow is varying for each call of program even
+for same test case.
+
+solution - elevation_angle and azimuth_angle were deg we needed to convert it
+to radians for trigonometric calculations
+'''
+obs_location=EarthLocation(lat=x*u.deg,lon=y*u.deg)
+altaz=sun.transform_to(AltAz(obstime=now,location=obs_location))
+elevation_angle = altaz.alt.deg
+azimuth_angle = altaz.az.deg
+
+
 shadow_points_and_height={}
 
-# we are not able to map correctly the user input coord with shadowed last coord
-# because dictionaries are ordered in python !!
 
 
-i=0
 for (a,b) in user_input:
-    shadow_height=(user_input[(a,b)]/np.tan(elevation_angle[i]))
-    x_shadow_coord=(a+shadow_height*np.sin(azimuth_angle[i]))
-    y_shadow_coord=(b+shadow_height*np.cos(azimuth_angle[i]))
+    shadow_height=(user_input[(a,b)]/np.tan(np.radians(elevation_angle)))
+    x_shadow_coord=(a+shadow_height*np.sin(np.radians(azimuth_angle)))
+    y_shadow_coord=(b+shadow_height*np.cos(np.radians(azimuth_angle)))
     shadow_points_and_height[x_shadow_coord,y_shadow_coord,a,b]=shadow_height
-    i+=1
 
 for (a, b, c, d) in shadow_points_and_height:
     print("obstacle location:", a, b)
